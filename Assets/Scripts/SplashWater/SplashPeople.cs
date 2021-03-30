f5a195a592795504f8b6ff6ee6b466a66c5e408f
Cansutunc <cansutunc00@gmail.com>
2021-03-30 21:23:35 +0300

    SplashedPeople

--- Assets/Scripts/SplashWater/SplashPeople.cs
+++ Assets/Scripts/SplashWater/SplashPeople.cs
@@ -4,8 +4,29 @@ using UnityEngine;
 
 public class SplashPeople : MonoBehaviour
 {
+    bool isPeopleFall;
+    [SerializeField] private List<GameObject> peoples;
+    private void Start()
+    {
+      
+    }
     private void OnParticleTrigger()
     {
         //TO DO: Ragdoll NPC's, People Multiplier Score ++(Random)
+        
     }
+    private void FallenPeople()
+    {
+        if (isPeopleFall) return;
+        GetComponentInChildren<Animator>().SetBool("isDead",true);
+        isPeopleFall = true;
+        Rigidbody rb = GetComponent<Rigidbody>();
+        rb.AddExplosionForce(300f, transform.forward, 300f);
+    }
+    private void OnParticleCollision()
+    {
+        FallenPeople();
+        Debug.Log("Fallen People");
+    }
+
 }
